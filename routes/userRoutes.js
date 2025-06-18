4ef78af39a4d934497ba6916e1b72056d548cab0
unidev0518 <unicorntalent0518@gmail.com>
2025-06-17 20:24:09 -0700

    add route for user api

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+// routes/userRoutes.js
+const express = require('express');
+const { getUserById } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.get('/:id', getUserById);
+
+module.exports = router;
